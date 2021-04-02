2fe37dac63906acb9fc905f4c93d4302523df66a
olteanuvlad <olteanu.vlad1997@gmail.com>
2021-04-02 22:41:38 +0300

    .

--- Homework06/Homework06/Program.cs
+++ Homework06/Homework06/Program.cs
@@ -8,6 +8,13 @@ namespace Homework06
     {
         static void Main(string[] args)
         {
+
+            IRepository<Brand> brandRepo_EF = new EFRepository<Brand>();
+            IRepository<Brand> brandRepo_Mongo = new MongoDbRepository<Brand>();
+
+            IRepository<CarModel> modelRepo_EF = new EFRepository<CarModel>();
+            IRepository<CarModel> modelRepo_Mongo = new MongoDbRepository<CarModel>();
+
             Brand mazda = new Brand
             {
                 Name = "Mazda"
@@ -18,29 +25,30 @@ namespace Homework06
                 Name = "BMW"
             };
 
+            brandRepo_EF.Insert(mazda);
+
+            var enumerator = brandRepo_EF.GetAll().GetEnumerator();
+            enumerator.MoveNext();
+            var current = enumerator.Current;
+
+
             CarModel cx5 = new CarModel
             {
                 Name = "CX-5",
-                BrandId = mazda.Id
+                BrandId = current.Id
             };
 
-            IRepository<Brand> brandRepo_EF = new EFRepository<Brand>();
-            IRepository<Brand> brandRepo_Mongo = new MongoDbRepository<Brand>();
+            enumerator.Dispose();
+            
+            //brandRepo_Mongo.Insert(mazda);
+            brandRepo_EF.Insert(bmw);
+            //brandRepo_Mongo.Insert(bmw);
 
-            IRepository<CarModel> modelRepo_EF = new EFRepository<CarModel>();
-            IRepository<CarModel> modelRepo_Mongo = new MongoDbRepository<CarModel>();
+            brandRepo_EF.Delete(bmw);
+            //brandRepo_Mongo.Delete(bmw);
 
-
-            //brandRepo_EF.Insert(mazda);
-            brandRepo_Mongo.Insert(mazda);
-           // brandRepo_EF.Insert(bmw);
-            brandRepo_Mongo.Insert(bmw);
-
-            //brandRepo_EF.Delete(bmw);
-            brandRepo_Mongo.Delete(bmw);
-
-            //modelRepo_EF.Insert(cx5);
-            modelRepo_Mongo.Insert(cx5);
+            modelRepo_EF.Insert(cx5);
+            //modelRepo_Mongo.Insert(cx5);
 
         }
     }
